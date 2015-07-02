b7699f50f5c28bd46d42dc46cb8be78288203647
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-07-02 09:55:24 +0200

    [TASK] Remove domain model Photo

--- ext_tables.sql
+++ ext_tables.sql
@@ -383,34 +383,6 @@ CREATE TABLE tx_easyvotesmartvote_domain_model_electionlist (
 );
 
 #
-# Table structure for table 'tx_easyvotesmartvote_domain_model_photo'
-#
-CREATE TABLE tx_easyvotesmartvote_domain_model_photo (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
-	file_name_and_path varchar(255) DEFAULT '' NOT NULL,
-	election int(11) unsigned DEFAULT '0',
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-
- KEY language (l10n_parent,sys_language_uid)
-
-);
-
-#
 # Table structure for table 'tx_easyvotesmartvote_domain_model_link'
 #
 CREATE TABLE tx_easyvotesmartvote_domain_model_link (
