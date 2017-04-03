bad9e5cffd08912f1347ed80f85c5082913ac8b1
Fabien Udriot <fabien@ecodev.ch>
2017-04-03 18:16:31 +0200

    [BUGFIX] Ensure existence of DOM element (refs #20)

--- Resources/Public/JavaScript/App/Views/Question/ListView.js
+++ Resources/Public/JavaScript/App/Views/Question/ListView.js
@@ -371,14 +371,17 @@ export default class ListView extends Backbone.View {
 		var url = '';
 		var sanitizedUrl = '';
 
-		sanitizedUrl = $('#btn-candidate-directory-1').attr('href').replace(/\#.+/, '');
-		url = sanitizedUrl + '#district=' + $('#btn-district').val();
-		$('#btn-candidate-directory-1').attr('href', url);
+		if ($('#btn-candidate-directory-1') && $('#btn-candidate-directory-1').attr('href')) {
+			sanitizedUrl = $('#btn-candidate-directory-1').attr('href').replace(/\#.+/, '');
+			url = sanitizedUrl + '#district=' + $('#btn-district').val();
+			$('#btn-candidate-directory-1').attr('href', url);
+		}
 
-
-		sanitizedUrl = $('#btn-candidate-directory-2').attr('href').replace(/\#.+/, '');
-		url = sanitizedUrl + '#district=' + $('#btn-district').val();
-		$('#btn-candidate-directory-2').attr('href', url);
+		if ($('#btn-candidate-directory-2') && $('#btn-candidate-directory-2').attr('href')) {
+			sanitizedUrl = $('#btn-candidate-directory-2').attr('href').replace(/\#.+/, '');
+			url = sanitizedUrl + '#district=' + $('#btn-district').val();
+			$('#btn-candidate-directory-2').attr('href', url);
+		}
 	}
 
 	/**
